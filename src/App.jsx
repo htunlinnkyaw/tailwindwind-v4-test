05a861402d13ffd6e9d2c9b936855de54230e74a
htunlinnkyaw <tunlinkyaw4781@gmail.com>
2025-01-28 22:07:13 +0630

    tailwind-v4 test file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="text-3xl h-screen bg-header">Header</div>
+      {/* <div className="grid grid-cols-3 h-[38.5rem]">
+        <div className="col-span-1 text-2xl  bg-amber-700">Side Bar</div>
+        <div className="col-span-2 text-2xl  bg-gray-500">Content</div>
+      </div> */}
+      {/* <div className="mt-auto text-3xl   bg-neutral-500">Footer</div> */}
+    </div>
+  );
+};
+
+export default App;
